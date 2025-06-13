bb6b945feb266a8ac00b9db24ec8616d66933452
kiv1n <aloha@kiv1n.ru>
2025-06-13 11:56:18 +0300

    Reorder mainnet chain configuration for clarity

--- config/chain.ts
+++ config/chain.ts
@@ -1,19 +1,19 @@
 import { ChainConfig } from "@/types/chain-config";
 import { lukso, luksoTestnet } from "viem/chains";
 
-const chainConfigMainnet: ChainConfig = {
-  chain: lukso,
-  contracts: {
-    question: "0x0000000000000000000000000000000000000000",
-    questionManager: "0x0000000000000000000000000000000000000000",
-  },
-};
-
 const chainConfigTestnet: ChainConfig = {
   chain: luksoTestnet,
   contracts: {
     question: "0xA958c128203d2671636f8820E943640A097e6A60",
     questionManager: "0xEDe254159220fEE61bC83FDa5A9f1EA5b510472e",
+  },
+};
+
+const chainConfigMainnet: ChainConfig = {
+  chain: lukso,
+  contracts: {
+    question: "0x0000000000000000000000000000000000000000",
+    questionManager: "0x0000000000000000000000000000000000000000",
   },
 };
 
